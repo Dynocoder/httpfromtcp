80693ee50c78346f8754c390f806b9266af91ee0
Dynocoder <sauravprashar21@gmail.com>
2025-11-17 18:18:40 -0500

    feature: chunked reading

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,14 +1,46 @@
 package request
 
 import (
+	"bytes"
 	"errors"
 	"io"
 	"strings"
 	"unicode"
 )
 
+type ParserState int
+
+const (
+	initialized ParserState = iota
+	done
+)
+const bufferSize = 8
+
 type Request struct {
 	RequestLine RequestLine
+	ParserState ParserState
+}
+
+func (r *Request) parse(data []byte) (int, error) {
+	if r.ParserState == initialized {
+		line, n, err := parseRequestLine(data)
+		if err != nil {
+			return 0, err
+		} else if n == 0 {
+			return 0, nil // did not receive CRLF, keep going
+		} else if n > 0 {
+			r.RequestLine = *line
+			r.ParserState = done
+			return n, nil
+		}
+
+	} else if r.ParserState == done {
+		return 0, errors.New("error: trying to read data in done state")
+	} else {
+		return 0, errors.New("error: Unknown state")
+	}
+
+	return 0, nil
 }
 
 type RequestLine struct {
@@ -18,40 +50,70 @@ type RequestLine struct {
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	data, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
+	buf := make([]byte, bufferSize, bufferSize)
+
+	request := &Request{
+		ParserState: initialized,
 	}
 
-	dataString := string(data)
+	readToIndex := 0
 
-	requestLine, err := parseRequestLine(dataString)
-	if err != nil {
-		return nil, err
+	for request.ParserState != done {
+
+		// if buffer is full, grow it
+		if readToIndex == cap(buf) {
+			newBuf := make([]byte, readToIndex, cap(buf)*2)
+			copy(newBuf, buf[:readToIndex])
+			buf = newBuf
+		}
+
+		n, err := reader.Read(buf[readToIndex:cap(buf)])
+		if n > 0 {
+			readToIndex += n
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		consumed, perr := request.parse(buf)
+		if perr != nil {
+			return nil, perr
+		}
+
+		if consumed > 0 {
+			copy(buf, buf[consumed:readToIndex])
+			readToIndex -= consumed
+		}
 	}
 
-	var request Request
-
-	request.RequestLine = *requestLine
-
-	return &request, nil
+	return request, nil
 }
 
-func parseRequestLine(data string) (*RequestLine, error) {
+func parseRequestLine(data []byte) (*RequestLine, int, error) {
 
-	lines := strings.Split(data, "\r\n")
-	requestLineParts := strings.Split(lines[0], " ")
+	idx := bytes.Index(data, []byte("\r\n"))
+
+	if idx == -1 {
+		return &RequestLine{}, 0, nil
+	}
+
+	line := string(data[:idx])
+	n := len(line)
+	requestLineParts := strings.Split(line, " ")
 
 	var requestLine RequestLine
 
 	// validate request line part count
 	if len(requestLineParts) != 3 {
-		return nil, errors.New("Request Line should have 3 fields")
+		return nil, n, errors.New("Request Line should have 3 fields")
 	}
 
 	//validate request method
 	if !isUppercaseAlphabetic(requestLineParts[0]) {
-		return nil, errors.New("Invalid Request Method")
+		return nil, n, errors.New("Invalid Request Method")
 	}
 	requestLine.Method = requestLineParts[0]
 
@@ -60,16 +122,16 @@ func parseRequestLine(data string) (*RequestLine, error) {
 	// validate http request version (only supports 1.1)
 	httpVersion := strings.Split(requestLineParts[2], "/")
 	if len(httpVersion) != 2 {
-		return nil, errors.New("Invalid HTTP Version")
+		return nil, n, errors.New("Invalid HTTP Version")
 	}
 
 	if httpVersion[1] != "1.1" {
-		return nil, errors.New("Invalid HTTP Version")
+		return nil, n, errors.New("Invalid HTTP Version")
 	}
 
 	requestLine.HttpVersion = httpVersion[1]
 
-	return &requestLine, nil
+	return &requestLine, n, nil
 
 }
 
