2dec4a9bd6656de7e3c3c4c310b4a599cae65f2c
Dynocoder <sauravprashar21@gmail.com>
2026-01-30 21:50:50 -0500

    added more test cases for header parsing

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -27,4 +27,36 @@ func TestHeadersParse(t *testing.T) {
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
 
+	// Test: Valid 2 headers with existing headers
+	headers = Headers{
+		"Existing": "Header",
+	}
+	data = []byte("Host: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "Header", headers["Existing"])
+	assert.Equal(t, 23, n)
+	assert.True(t, done)
+
+	// Test: Valid incomplete headers
+	headers = NewHeaders()
+	data = []byte("Host: localhost:42069\r\nContent-Type: text/html\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "text/html", headers["Content-Type"])
+	assert.Equal(t, 48, n)
+	assert.False(t, done)
+
+	// Test:  Invalid spacing header
+	headers = NewHeaders()
+	data = []byte("Host : localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
 }
